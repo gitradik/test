30343478119235abf2a812c84b12707ab703d75f
Родион Малахов <rminzp17@gmail.com>
2019-05-24 07:46:37 +0300

    navbar

--- index.php
+++ index.php
@@ -49,7 +49,23 @@
                         </div>
                         <div class="col-md-10 d-flex justify-content-end align-items-end">
                             <div class="nav-menu">
-                             <!--TODO-->
+                                <ul class="nav-items">
+                                    <?php
+                                    $fields = get_fields(31);
+                                    if( $fields ):
+                                        ?>
+                                        <?php
+                                        foreach ($fields as $field_name => $field):
+                                            if( $field ):
+                                                ?>
+                                                <li class="<?= $field_name == 'subscribe' ? 'active-link' : ''?>">
+                                                    <p><?= $field?></p>
+                                                </li>
+                                            <?php endif;?>
+                                        <?php endforeach; ?>
+
+                                    <?php endif; ?>
+                                </ul>
                             </div>
                         </div>
                         <div class="col-md-1 d-flex justify-content-end align-items-center">
